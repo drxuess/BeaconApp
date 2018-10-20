23ed78488940ba63b9075490a14f2b6fe91c745e
Morgan Xu <morgan.xu8@gmail.com>
2018-10-20 18:56:44 +1100

    setting up database

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs')
+const setup = require('./setup')
+
+if (!fs.existsSync('database.json')) {
+    setup.setupDB()
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
