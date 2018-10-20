dd0cfacc9513c7392d306a25ecee3545cb58654c
Leo Dong <Leo.dong@hotmail.com.au>
2018-10-20 19:00:22 +1100

    fixed data gathering script

--- scripts/get_fires.sh
+++ scripts/get_fires.sh
@@ -1,1 +1,10 @@
+dir='../data/FIRMS/c6/Australia_NewZealand/'
+
+rm -rf ../data/
+
 wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=4 "https://nrt4.modaps.eosdis.nasa.gov/api/v2/files/contents/FIRMS/c6/Australia_NewZealand" --header "Authorization: Bearer B44112B0-D42E-11E8-A6EB-CF089B439298" -P  wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=4 "https://nrt4.modaps.eosdis.nasa.gov/api/v2/files/contents/FIRMS/c6/Australia_NewZealand" --header "Authorization: Bearer B44112B0-D42E-11E8-A6EB-CF089B439298" -P ../data
+
+newest=$(ls -1 $dir | tail -n 1)
+
+echo $newest
+
