5002a405f0cbb6d1cb565a39db7d81ca8fa14ac5
Leo Dong <Leo.dong@hotmail.com.au>
2018-10-21 13:55:40 +1100

    fire to postcode added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3000
 const fs = require('fs')
 const loki = require("lokijs")
 
+var Distance = require('geo-distance');
+
 const db = new loki("database.json")
 
 //SETUP
@@ -34,8 +36,35 @@ function startServer() {
     app.get('/fireinfo', function (req, res) {
         var postcode = req.query.postcode
         var postcodes = db.getCollection("postcodes")
+		var fires = db.getCollection('fires')
+		var min_dist = 100000000000;
+		var closest_fire = null;
+
+		var primary = postcodes.find({'postcode': { '$eq' : parseInt(postcode) }});
+		var search = fires.find({'latitude': { '$ne' : null }});
+
+		for(var i=0; i < search.length; i++){
+
+	  		var to = {
+	  			lat: search[i].latitude,
+	  			lon: search[i].longitude
+	  		}
+
+	  		var from = {
+	  			lat: primary[0].lat,
+	  			lon: primary[0].lon
+	  		}
+	  		
+	  		var geomDist = Distance.between(to, from).human_readable().distance;
+
+	  		if (min_dist > geomDist) {
+	  			min_dist = Math.min(min_dist, geomDist)
+	  			closest_fire = i
+	  		}
+	  	}
+
         var location = postcodes.findOne({postcode: postcode})
-        res.send(postcode)
+        res.send([min_dist, fires.get(closest_fire)])
     })
     app.get('/signup', function (req, res) {
         res.sendFile(__dirname + "/web/signup.html")
--- closest_fire_to_postcode.js
+++ closest_fire_to_postcode.js
@@ -1,24 +1,24 @@
 const loki = require("lokijs")
 
-// var distance = require('euclidean-distance')
 var Distance = require('geo-distance');
 var db = new loki("database.json")
+var wait = ms => new Promise((r, j)=>setTimeout(r, ms))
 
 function getClosestFireDist(postcode) {
-	db.loadDatabase({}, function () {
+	var min_dist = 100000000000;
+	var closest_fire = null;
+
+	var test = db.loadDatabase({}, function () {
 		var postcodes = db.getCollection('postcodes')
 		var fires = db.getCollection('fires')
 
 
 		var primary = postcodes.find({'postcode': { '$eq' : postcode }});
 		var search = fires.find({'latitude': { '$ne' : null }});
-		var min_dist = 100000000000;
 
 
-		// for(var i=0; i < search.length; i++){
-			for(var i=0; i < search.length; i++){
+		for(var i=0; i < search.length; i++){
 
-	  		// distance([search[i].latitude, search[i].longitude], [primary[0].lat,primary[0].lon])
 	  		var to = {
 	  			lat: search[i].latitude,
 	  			lon: search[i].longitude
@@ -28,17 +28,20 @@ function getClosestFireDist(postcode) {
 	  			lat: primary[0].lat,
 	  			lon: primary[0].lon
 	  		}
-
 	  		
 	  		var geomDist = Distance.between(to, from).human_readable().distance;
-	  		min_dist = Math.min(min_dist, geomDist)
 
-
+	  		if (min_dist > geomDist) {
+	  			min_dist = Math.min(min_dist, geomDist)
+	  			closest_fire = i
+	  		}
 	  	}
-	  	console.log(min_dist)
-
+	  	console.log([min_dist, closest_fire])
 	});
+	// console.log(test)
+	// return test
 
 }
 
+//console.log(getClosestFireDist(2000))
 getClosestFireDist(2000)
