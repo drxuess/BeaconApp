dfef71e5373da828b90290c643cc864c4cc36aae
Leo Dong <Leo.dong@hotmail.com.au>
2018-10-21 11:02:14 +1100

    message server set up

--- send_sms.js
+++ send_sms.js
@@ -3,12 +3,42 @@
 const accountSid = 'AC94c8ce4abe01e424ddb781e63dd8ed24';
 const authToken = '5055e904607ab9cba167aab310bedb8a';
 const client = require('twilio')(accountSid, authToken);
+const toMobile = '+61468619908'
+const fromMobile = '+61428170836'
 
-client.messages
-  .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+61428170836',
-     to: '+61468619908'
-   })
-  .then(message => console.log(message.sid))
-  .done();
+function alert(to, from) {
+    client.messages
+	  .create({
+	     body: 'A fire has been registered near your area. See here for more details: http://www.beac.on/sh923hgs',
+	     from: from,
+	     to: to
+	   })
+	  .then(message => console.log(message.sid))
+	  .done();
+}
+
+function confirmation(to, from) {
+	client.messages
+	  .create({
+	     body: 'A fire has been registered near your area. See here for more details: http://www.beac.on/sh923hgs',
+	     from: from,
+	     to: to
+	   })
+	  .then(message => console.log(message.sid))
+	  .done();
+}
+
+function warning(to, from) {
+	client.messages
+	  .create({
+	     body: 'There is a fire VERY CLOSE to your postcode. Please consider evacuating! http://www.beac.on/sh923hgs',
+	     from: from,
+	     to: to
+	   })
+	  .then(message => console.log(message.sid))
+	  .done();
+}
+
+alert(toMobile, fromMobile)
+confirmation(toMobile, fromMobile)
+warning(toMobile, fromMobile)
