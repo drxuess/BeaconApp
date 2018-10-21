9aa4ef6f41d32b947cfd87056993e61c4c8fcdcf
Morgan Xu <morgan.xu8@gmail.com>
2018-10-21 12:11:30 +1100

    Sign up page

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const port = 3000
 const fs = require('fs')
 const loki = require("lokijs")
 
+const db = new loki("database.json")
+
 //SETUP
 if (!fs.existsSync('database.json')) {
-    var db = new loki("database.json")
     var users = db.addCollection('users')
     var fires = db.addCollection('fires')
     var postcodes = db.addCollection('postcodes')
@@ -19,7 +20,9 @@ if (!fs.existsSync('database.json')) {
         startServer()
     })
 } else {
-    startServer()
+    db.loadDatabase({}, function(){
+        startServer()
+    })
 }
 
 function startServer() {
@@ -30,7 +33,29 @@ function startServer() {
     app.get('/', (req, res) => res.sendFile(__dirname + "/web/home.html"))
     app.get('/fireinfo', function (req, res) {
         var postcode = req.query.postcode
+        var postcodes = db.getCollection("postcodes")
+        var location = postcodes.findOne({postcode: postcode})
         res.send(postcode)
     })
+    app.get('/signup', function (req, res) {
+        res.sendFile(__dirname + "/web/signup.html")
+    })
+    app.get('/register', function (req, res) {
+        var users = db.getCollection("users")
+        var number = req.query.number
+        var postcode = req.query.postcode
+        try {
+            users.insert({mobileNumber: number, postcode: postcode})
+            db.save()
+            res.send("Success")
+        } catch (error) {
+            res.send("Fail")
+        }
+        
+    })
     app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+}
+
+function checkNearbyFire(postcode) {
+    
 }
--- web/js/home.js
+++ web/js/home.js
@@ -11,3 +11,9 @@ function searchArea(){
   var postCode = $("#postcode").val()
   window.location.href = "/fireinfo?postcode=" + postCode
 }
+
+function registerNumber(){
+  var number = $('#number').val()
+  var postcode = $('#postcode').val()
+  window.location.href = "/register?number=" + number + "&postcode=" + postcode
+}
